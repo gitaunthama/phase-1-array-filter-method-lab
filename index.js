7f3dad9ae3fe17f893e62433f00bf6498b0e5428
gitaunthama <gitaunthama@gmail.com>
2024-03-25 19:00:16 +0300

    complete work

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution here
+const drivers = [
+    [{
+        "id": 1,
+        "name": "Bobby",
+        "gender": "Male"
+      }, {
+        "id": 2,
+        "name": "Sammy",
+        "gender": "Male"
+      }, {
+        "id": 3,
+        "name": "Sally",
+        "gender": "Female"
+      }, {
+        "id": 4,
+        "name": "Annette",
+        "gender": "Female"
+      }, {
+        "id": 5,
+        "name": "Sarah ",
+        "gender": "Female"
+      },{
+          "id": 5,
+          "name": "Bobby",
+          "gender": "Female"
+        }
+      
+
+    ]]
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => 
+    driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => typeof driver === 'string' && driver.startsWith(letters));
+}
+
+
+
+
+
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+
+ 
+  
+
